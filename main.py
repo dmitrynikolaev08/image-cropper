8805d3cd75a8cdf0e8db6ecbdc7e99d1693f911f
Dmitrii Nikolaev <dmitrynikolaev08@gmail.com>
2023-12-06 18:07:35 +0100

    multiple images can be cropped

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
+import os
 
 
-def crop_object(image_path, output_path, background_margin=0):
+def crop_object(image_path, output_path, background_margin=25, background_color=(255, 255, 255)):
     # Open the image
     image = Image.open(image_path)
     # Convert the image to grayscale for easier processing
@@ -24,51 +25,55 @@ def crop_object(image_path, output_path, background_margin=0):
     right = min(img_array.shape[1], right + background_margin)
     # Crop the image
     cropped_image = image.crop((left, top, right, bottom))
-    # Convert 'RGBA' to 'RGB' if necessary
+
+    bg_image = Image.new('RGB', cropped_image.size, background_color)
+
     if cropped_image.mode == 'RGBA':
-        cropped_image = cropped_image.convert('RGB')
-    cropped_image.save(output_path)
+        # Extract the alpha channel as a mask
+        mask = cropped_image.split()[3]
+        bg_image.paste(cropped_image.convert('RGB'), (0, 0), mask)
+    else:
+        bg_image.paste(cropped_image, (0, 0))
 
-    cropped_image.save(output_path)
+    # Save the image with the new background
+    bg_image.save(output_path)
 
 
-def select_image():
-    # Select an image file
-    path = filedialog.askopenfilename()
-    if len(path) > 0:
-        global img_path
-        img_path = path
-        # Load and display the image
-        image = Image.open(path)
+def select_images():
+    # Select multiple image files
+    paths = filedialog.askopenfilenames()
+    global img_paths
+    img_paths = list(paths)
+    if img_paths:
+        # Load and display the first selected image
+        image = Image.open(img_paths[0])
         image = image.resize((250, 250), Image.Resampling.LANCZOS)
         photo = ImageTk.PhotoImage(image)
         panel.configure(image=photo)
         panel.image = photo
 
 
-def process_image():
-    if img_path:
-        output_path = "output.jpg"  # You can modify this path
-        crop_object(img_path, output_path)
-        result_label.config(text=f"Processed image saved as {output_path}")
+def process_images():
+    if img_paths:
+        for img_path in img_paths:
+            file_name = os.path.basename(img_path)
+            output_path = file_name
+            crop_object(img_path, output_path)
+        result_label.config(text=f"Processed {len(img_paths)} images.")
 
 
 # Initialize Tkinter window
 window = tk.Tk()
 window.title("Background Remover")
 
-# Initialize path variable
-img_path = ""
-
 # Create a panel to display the image
 panel = tk.Label(window)
 panel.pack(padx=10, pady=10)
 
 # Create buttons
-btn_select = tk.Button(window, text="Select Image", command=select_image)
+btn_select = tk.Button(window, text="Select Images", command=select_images)
+btn_process = tk.Button(window, text="Crop Images", command=process_images)
 btn_select.pack(side="left", padx=10, pady=10)
-
-btn_process = tk.Button(window, text="Crop Image", command=process_image)
 btn_process.pack(side="right", padx=10, pady=10)
 
 # Label to display result message
